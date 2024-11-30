366eb7878b2b6dc4405c900b40ce60d1aa27f1d2
Aleksandras Naruševič <aleksandr.narusevic@stxgroup.com>
2024-11-30 15:42:58 +0200

    Added pg sql configuration

--- app/database.py
+++ app/database.py
@@ -1,9 +1,13 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost/db_name"
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"  # Using SQLite for simplicity
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"  
+# Using SQLite for simplicity
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
